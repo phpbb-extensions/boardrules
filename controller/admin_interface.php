7ca4f04a9d189a21e6e90dfde53cdf6cad778706
Nathan Guse <nathaniel.guse@gmail.com>
2013-09-12 11:43:16 -0500

    Controller interface classes (without methods)

---
+++ controller/admin_interface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+*
+* @package Board Rules Extension
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* Interface for our admin controller
+*
+* This describes all of the methods we'll use for the admin front-end of this
+*	extension
+*/
+class phpbb_ext_phpbb_boardrules_controller_admin_interface
+{
+}
