74823d401b5ef74b9ba5eed1b72f16f374c68393
Nathan Guse <nathaniel.guse@gmail.com>
2013-09-12 11:48:08 -0500

    Extension class (for enable/disable/purge) (empty)

---
+++ ext.php
@@ -0,0 +1,15 @@
+<?php
+/**
+*
+* @package Board Rules Extension
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* Extension class for custom enable/disable/purge actions
+*/
+class phpbb_ext_phpbb_boardrules_ext extends phpbb_extension_base
+{
+}
