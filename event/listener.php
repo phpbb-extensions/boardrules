bdda110a41d7da96b0458ed89eada7bf4858a5d9
Matt Friedman <maf675@gmail.com>
2013-10-21 15:20:46 -0700

    Create an event listener

---
+++ event/listener.php
@@ -0,0 +1,30 @@
+<?php
+/**
+*
+* @package Board Rules Extension
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbb\boardrules\event;
+
+/**
+* @ignore
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* Event listener
+*/
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class listener implements EventSubscriberInterface
+{
+
+
+}
