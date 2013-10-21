a5cc6daa867a11c20a240ed4eaee821fc5b698d1
Matt Friedman <maf675@gmail.com>
2013-10-21 15:23:38 -0700

    Add language files / code for the Rules header link

--- event/listener.php
+++ event/listener.php
@@ -26,5 +26,23 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class listener implements EventSubscriberInterface
 {
 
+	// Assign functions defined in this class to event listeners in the core
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.user_setup'	=> 'load_language_on_setup',
+		);
+	}
+
+	// Load common board rules language files during user setup
+	public function load_language_on_setup($event)
+	{
+		$lang_set_ext = $event['lang_set_ext'];
+		$lang_set_ext[] = array(
+			'ext_name' => 'phpbb/boardrules',
+			'lang_set' => 'boardrules',
+		);
+		$event['lang_set_ext'] = $lang_set_ext;
+	}
 
 }
