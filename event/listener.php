6c5c5520379f76e8f969e761454f796d9a393519
Matt Friedman <maf675@gmail.com>
2013-11-13 14:31:04 -0800

    Reorder dockblock

--- event/listener.php
+++ event/listener.php
@@ -25,8 +25,8 @@ class listener implements EventSubscriberInterface
 	/**
 	* Constructor
 	* 
-	* @param \phpbb\template\template    $template           Template object
 	* @param \phpbb\controller\helper    $controller_helper  Controller helper object
+	* @param \phpbb\template\template    $template           Template object
 	*/
 	public function __construct(\phpbb\controller\helper $controller_helper, \phpbb\template\template $template)
 	{
