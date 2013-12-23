b7fda90c9ceb550a38d0e4d4b7c73328514f4406
Nathaniel Guse <nathaniel.guse@gmail.com>
2013-12-23 10:44:59 -0600

    Get/set message & related functions

--- entity/rule.php
+++ entity/rule.php
@@ -79,4 +79,194 @@ class rule implements rule_interface
 		// Return $this; so calls can be chained load()->set()->save()
 		return $this;
 	}
+
+	/**
+	* Get message for edit
+	*
+	* @return string
+	* @access public
+	*/
+	public function get_message_for_edit()
+	{
+		// If these haven't been set yet; use defaults
+		$text = (isset($this->data['rule_message'])) ? $this->data['rule_message'] : '';
+		$uid = (isset($this->data['rule_message_bbcode_uid'])) ? $this->data['rule_message_bbcode_uid'] : '';
+		$options = (isset($this->data['rule_message_bbcode_options'])) ? (int) $this->data['rule_message_bbcode_options'] : 0;
+
+		// Generate for edit
+		$message_data = generate_text_for_edit($text, $uid, $options);
+
+		return $message_data['text'];
+	}
+
+	/**
+	* Get message for display
+	*
+	* @param bool $censor_text True to censor the text (Defaut: true)
+	* @return string
+	* @access public
+	*/
+	public function get_message_for_display($censor_text = true)
+	{
+		// If these haven't been set yet; use defaults
+		$text = (isset($this->data['rule_message'])) ? $this->data['rule_message'] : '';
+		$uid = (isset($this->data['rule_message_bbcode_uid'])) ? $this->data['rule_message_bbcode_uid'] : '';
+		$bitfield = (isset($this->data['rule_message_bbcode_bitfield'])) ? $this->data['rule_message_bbcode_bitfield'] : '';
+		$options = (isset($this->data['rule_message_bbcode_options'])) ? (int) $this->data['rule_message_bbcode_options'] : 0;
+
+		// Generate for display
+		return generate_text_for_display($text, $uid, $bitfield, $options, $censor_text);
+	}
+
+	/**
+	* Set message
+	*
+	* @param string $message
+	* @return rule_interface $this
+	* @access public
+	* @throws \phpbb\boardrules\exception\base
+	*/
+	public function set_message($message)
+	{
+		// Prepare the text for storage
+		$uid = $bitfield = $flags = '';
+		generate_text_for_storage($message, $uid, $bitfield, $flags, $this->message_bbcode_enabled(), $this->message_magic_url_enabled(), $this->message_smilies_enabled());
+
+		// Set the message to our data array
+		$this->data['rule_message'] = $message;
+		$this->data['rule_message_bbcode_uid'] = $uid;
+		$this->data['rule_message_bbcode_bitfield'] = $bitfield;
+		// Flags are already set
+
+		// Return $this; so calls can be chained load()->set()->save()
+		return $this;
+	}
+
+	/**
+	* Check if bbcode is enabled on the message
+	*
+	* @return bool
+	* @access public
+	*/
+	public function message_bbcode_enabled()
+	{
+		return ($this->data['rule_message_bbcode_options'] & OPTION_FLAG_BBCODE);
+	}
+
+	/**
+	* Enable bbcode on the message
+	*
+	* @return null
+	* @access public
+	*/
+	public function message_enable_bbcode()
+	{
+		$this->data['rule_message_bbcode_options'] = $this->set_option($this->data['rule_message_bbcode_options'], OPTION_FLAG_BBCODE);
+	}
+
+	/**
+	* Disable bbcode on the message
+	*
+	* @return null
+	* @access public
+	*/
+	public function message_disable_bbcode()
+	{
+		$this->data['rule_message_bbcode_options'] = $this->set_option($this->data['rule_message_bbcode_options'], OPTION_FLAG_BBCODE, true);
+	}
+
+	/**
+	* Check if magic_url is enabled on the message
+	*
+	* @return bool
+	* @access public
+	*/
+	public function message_magic_url_enabled()
+	{
+		return ($this->data['rule_message_bbcode_options'] & OPTION_FLAG_LINKS);
+	}
+
+	/**
+	* Enable magic url on the message
+	*
+	* @return null
+	* @access public
+	*/
+	public function message_enable_magic_url()
+	{
+		$this->data['rule_message_bbcode_options'] = $this->set_option($this->data['rule_message_bbcode_options'], OPTION_FLAG_LINKS);
+	}
+
+	/**
+	* Disable magic url on the message
+	*
+	* @return null
+	* @access public
+	*/
+	public function message_disable_magic_url()
+	{
+		$this->data['rule_message_bbcode_options'] = $this->set_option($this->data['rule_message_bbcode_options'], OPTION_FLAG_LINKS, true);
+	}
+
+	/**
+	* Check if smilies are enabled on the message
+	*
+	* @return bool
+	* @access public
+	*/
+	public function message_smilies_enabled()
+	{
+		return ($this->data['rule_message_bbcode_options'] & OPTION_FLAG_SMILIES);
+	}
+
+	/**
+	* Enable smilies on the message
+	*
+	* @return null
+	* @access public
+	*/
+	public function message_enable_smilies()
+	{
+		$this->data['rule_message_bbcode_options'] = $this->set_option($this->data['rule_message_bbcode_options'], OPTION_FLAG_SMILIES);
+	}
+
+	/**
+	* Disable smilies on the message
+	*
+	* @return null
+	* @access public
+	*/
+	public function message_disable_smilies()
+	{
+		$this->data['rule_message_bbcode_options'] = $this->set_option($this->data['rule_message_bbcode_options'], OPTION_FLAG_SMILIES, true);
+	}
+
+	/**
+	* Set option helper
+	*
+	* @param int $options Current options setting
+	* @param int $option_value Value of the option
+	* @param bool $negate Negate (unset) option (Default: False)
+	* @return int new options with value set
+	* @access protected
+	*/
+	protected function set_option($options, $option_value, $negate = false)
+	{
+		// If we're setting the option and the option is not already set
+		if (!$negate && !($option_field & $option_value))
+		{
+			// Add the option to the options
+			$options += $option_value;
+		}
+
+		// If we're unsetting the option and the option is already set
+		if ($negate && $option_field & $option_value)
+		{
+			// Subtract the option from the options
+			$options -= $option_value;
+		}
+
+		// Return the new options
+		return $options;
+	}
 }
