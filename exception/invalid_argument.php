0e4da94d94c5419ea4bbbbf74aa249b03dbd165e
Nathan Guse <nathaniel.guse@gmail.com>
2014-01-06 23:51:45 -0600

    InvalidArgument exception (if missing/invalid argument is sent)

---
+++ exception/invalid_argument.php
@@ -0,0 +1,17 @@
+<?php
+/**
+*
+* @package Board Rules Extension
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbb\boardrules\exception;
+
+/**
+* InvalidArgument exception
+*/
+class invalid_argument extends base
+{
+}
