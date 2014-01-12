83f293b6d5834068c9aebaa6ea06a01fc6fdf4cf
Matt Friedman <maf675@gmail.com>
2014-01-11 18:06:10 -0800

    Avoid jQuery conflicts

--- styles/prosilver/template/boardrules_controller.js
+++ styles/prosilver/template/boardrules_controller.js
@@ -1,17 +1,20 @@
-$(document).ready(function() {
+(function($, window, document) {  // Avoid conflicts with other libraries
 
-	// Apply highlight on page load
-	highlight(window.location.hash.substring(1));
+	$(document).ready(function () {
 
-	// Apply highlight to clicked rule anchor
-	$(".rule-anchor").on("click", function() {
-		$("li").removeClass("highlight");
-		highlight($(this).closest(".rule-item").attr("id"));		
+		// Apply highlight on page load
+		highlight(window.location.hash.substring(1));
+
+		// Apply highlight to clicked rule anchor
+		$(".rule-anchor").on("click", function () {
+			$("li").removeClass("highlight");
+			highlight($(this).closest(".rule-item").attr("id"));
+		});
+
+		// Function to apply highlight class to an element identifier
+		function highlight(id) {
+			$("#" + id).addClass("highlight");
+		}
+
 	});
-
-	// Function to apply highlight class to an element identifier
-	function highlight(id) {
-		$("#" + id).addClass("highlight");
-	}
-
-});
+})(jQuery, window, document);
