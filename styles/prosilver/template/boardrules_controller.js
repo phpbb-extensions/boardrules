214aa444b672533f5a0886d90d2b200128d19da6
Matt Friedman <maf675@gmail.com>
2014-01-11 16:06:52 -0800

    Add JS file for front end controller prosilver template

---
+++ styles/prosilver/template/boardrules_controller.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	// Apply highlight on page load
+	highlight(window.location.hash.substring(1));
+
+	// Apply highlight to clicked rule anchor
+	$(".rule-anchor").on("click", function() {
+		$("li").removeClass("highlight");
+		highlight($(this).closest(".rule-item").attr("id"));		
+	});
+
+	// Function to apply highlight class to an element identifier
+	function highlight(id) {
+		$("#" + id).addClass("highlight");
+	}
+
+});
