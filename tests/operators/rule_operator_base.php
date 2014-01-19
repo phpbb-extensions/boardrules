e2eb06e859eac6c68a785589616af09b57811fc3
Matt Friedman <maf675@gmail.com>
2014-01-19 11:02:15 -0800

    Add tests for rule operators

---
+++ tests/operators/rule_operator_base.php
@@ -0,0 +1,39 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbb\boardrules\tests\operators;
+
+/**
+* Base rule operator test (helper)
+*/
+class rule_operator_base extends \phpbb_database_test_case
+{
+	/**
+	* Get the rule operator
+	*
+	* @return \phpbb\boardrules\operators\rule
+	* @access protected
+	*/
+	protected function get_rule_operator()
+	{
+		return new \phpbb\boardrules\operators\rule();
+	}
+
+	public function getDataSet()
+	{
+		return $this->createXMLDataSet(dirname(__FILE__) . '/fixtures/rule.xml');
+	}
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->db = $this->new_dbal();
+	}
+}
