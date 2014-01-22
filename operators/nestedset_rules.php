bab0d09d8fa45fc7ed9a541f22ab3dc14204aed4
Matt Friedman <maf675@gmail.com>
2014-01-22 08:42:55 -0800

    Update the language function used in nested set class

--- operators/nestedset_rules.php
+++ operators/nestedset_rules.php
@@ -9,6 +9,9 @@
 
 namespace phpbb\boardrules\operators;
 
+/**
+* Nestedset class for Board Rules
+*/
 class nestedset_rules extends \phpbb\tree\nestedset
 {
 	/**
@@ -17,18 +20,17 @@ class nestedset_rules extends \phpbb\tree\nestedset
 	* @param \phpbb\db\driver\driver	$db		Database connection
 	* @param \phpbb\lock\db		$lock	Lock class used to lock the table when moving forums around
 	* @param string				$table_name		Table name
-	* @param int				$language		The language selection identifier; default: 0
+	* @return \phpbb\boardrules\operators\nestedset_rules
+	* @access public
 	*/
-	public function __construct(\phpbb\db\driver\driver $db, \phpbb\lock\db $lock, $table_name, $language = 0)
+	public function __construct(\phpbb\db\driver\driver $db, \phpbb\lock\db $lock, $table_name)
 	{
-		$this->language = $language;
-
 		parent::__construct(
 			$db,
 			$lock,
 			$table_name,
 			'RULES_NESTEDSET_',
-			'%srule_language = ' . $this->language,
+			'',
 			array(),
 			array(
 				'item_id'		=> 'rule_parent_id',
@@ -39,9 +41,16 @@ class nestedset_rules extends \phpbb\tree\nestedset
 		);
 	}
 
+	/**
+	* Assign the language id to the sql_where statement
+	*
+	* @param int					$language		The language selection identifier; default: 0
+	* @return nestedset_interface	$this
+	* @access public
+	*/
 	public function use_language($language)
 	{
-		$this->language = (int) $language;
+		$this->sql_where = '%srule_language = ' . (int) $language;
 		return $this;
 	}
 }
