fa908569b3224e92ff3521844a4e6663cd8f15a5
Matt Friedman <maf675@gmail.com>
2014-01-31 00:43:16 -0800

    Add tests

---
+++ tests/entity/rule_entity_left_id_test.php
@@ -0,0 +1,68 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbb\boardrules\tests\entity;
+
+/**
+* Tests related to tree ids on rule entity
+*/
+class rule_entity_left_id_test extends rule_entity_base
+{
+	/**
+	* Test data for the test_id() function
+	*
+	* @return array Array of test data
+	* @access public
+	*/
+	public function test_id_data()
+	{
+		return array(
+			array(
+				array(
+					'rule_id' => 1,
+				),
+				1,
+			),
+			array(
+				array(
+					'rule_id' => 3,
+				),
+				3,
+			),
+			array(
+				array(
+					'rule_id' => null,
+				),
+				0,
+			),
+			array(
+				array(),
+				0,
+			),
+		);
+	}
+
+	/**
+	* Test getting id
+	*
+	* @dataProvider test_id_data
+	* @access public
+	*/
+	public function test_id($data, $expected)
+	{
+		// Setup the entity class
+		$entity = $this->get_rule_entity();
+
+		// Set the data
+		$entity->import($data);
+
+		// Assert that the id matches what is expected
+		$this->assertEquals($expected, $entity->get_left_id());
+	}
+}
