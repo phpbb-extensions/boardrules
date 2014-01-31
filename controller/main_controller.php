9887600b8935c0ff1efc07c2becc2beeb4aaafe3
Matt Friedman <maf675@gmail.com>
2014-01-31 15:49:33 -0800

    Add get_rules function to the controller display function

--- controller/main_controller.php
+++ controller/main_controller.php
@@ -20,6 +20,9 @@ class main_controller implements main_interface
 	/** @var \phpbb\controller\helper */
 	protected $helper;
 
+	/** @var \phpbb\controller\helper */
+	protected $rule_operator;
+
 	/** @var \phpbb\template\template */
 	protected $template;
 
@@ -36,18 +39,20 @@ class main_controller implements main_interface
 	/**
 	* Constructor
 	*
-	* @param \phpbb\config\config        $config             Config object
-	* @param \phpbb\controller\helper    $helper             Controller helper object
-	* @param \phpbb\template\template    $template           Template object
-	* @param \phpbb\user                 $user               User object
-	* @param string                      $boardrules_table   Name of the table used to store boardrules data
+	* @param \phpbb\config\config                $config             Config object
+	* @param \phpbb\controller\helper            $helper             Controller helper object
+	* @param \phpbb\boardrules\operators\rule    $rule_operator      Rule operator object
+	* @param \phpbb\template\template            $template           Template object
+	* @param \phpbb\user                         $user               User object
+	* @param string                              $boardrules_table   Name of the table used to store boardrules data
 	* @return \phpbb\boardrules\controller\main_controller
 	* @access public
 	*/
-	public function __construct(\phpbb\config\config $config, \phpbb\controller\helper $helper, \phpbb\template\template $template, \phpbb\user $user, $boardrules_table)
+	public function __construct(\phpbb\config\config $config, \phpbb\controller\helper $helper, \phpbb\boardrules\operators\rule $rule_operator, \phpbb\template\template $template, \phpbb\user $user, $boardrules_table)
 	{
 		$this->config = $config;
 		$this->helper = $helper;
+		$this->rule_operator = $rule_operator;
 		$this->template = $template;
 		$this->user = $user;
 		$this->$boardrules_table = $boardrules_table;
@@ -63,6 +68,37 @@ class main_controller implements main_interface
 	{
 		// Add boardrules controller language file
 		$this->user->add_lang_ext('phpbb/boardrules', 'boardrules_controller');
+
+		$cat_counter = 1; // Numeric counter used for categories
+		$rule_counter = 'a'; // Alpha counter used for rules
+
+		// Grab all the rules in the current users language
+		$entities = $this->rule_operator->get_rules($this->user->get_iso_lang_id());
+
+		foreach ($entities as $entity)
+		{
+			// Rule categories
+			if ($entity->get_right_id() - $entity->get_left_id() > 1)
+			{
+				$this->template->assign_block_vars('categories', array(
+					'TITLE'		=> $entity->get_title(),
+					'U_ANCHOR'	=> $entity->get_anchor() ?: $this->user->lang('BOARDRULES_CATEGORY_ANCHOR', $cat_counter),
+				));
+
+				$cat_counter++;
+				$rule_counter = 'a';
+
+				continue;
+			}
+
+			// Rules
+			$this->template->assign_block_vars('categories.rules', array(
+				'MESSAGE'	=> $entity->get_message_for_display(),
+				'U_ANCHOR'	=> $entity->get_anchor() ?: $this->user->lang('BOARDRULES_RULE_ANCHOR', (($cat_counter - 1) . $rule_counter)),
+			));
+			
+			$rule_counter++;
+		}
 
 		$this->template->assign_vars(array(
 			'S_BOARDRULES'			=> true,
