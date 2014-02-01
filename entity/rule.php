26f49797a2e61e310532a74d509552ba021de2bb
Matt Friedman <maf675@gmail.com>
2014-01-31 16:03:54 -0800

    Fix type (left -> right)

--- entity/rule.php
+++ entity/rule.php
@@ -414,7 +414,7 @@ class rule implements rule_interface
 	/**
 	* Get the right identifier (for the tree)
 	*
-	* @return int left identifier
+	* @return int right identifier
 	* @access public
 	*/
 	public function get_right_id()
