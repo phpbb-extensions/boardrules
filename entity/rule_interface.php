9805f985c4760ff3cd509aae916d5220b0cfe04c
Matt Friedman <maf675@gmail.com>
2014-02-02 11:16:24 -0800

    Remove unneeded right/left_id from insert function
    These are not needed now that we use the phpbb/tree/nestedset class

--- entity/rule_interface.php
+++ entity/rule_interface.php
@@ -46,13 +46,11 @@ interface rule_interface
 	* Will throw an exception if the rule was already inserted (call save() instead)
 	*
 	* @param int $language The language identifier
-	* @param int $left_id The left id for the tree
-	* @param int $right_id The right id for the tree
 	* @return rule_interface $this
 	* @access public
 	* @throws \phpbb\boardrules\exception\base
 	*/
-	public function insert($language = 0, $left_id = 0, $right_id = 0);
+	public function insert($language = 0);
 
 	/**
 	* Save the current settings to the database
