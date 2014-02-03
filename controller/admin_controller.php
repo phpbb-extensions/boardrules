4037a01cfba337a3b89a3cdaedbd0deca5f82f62
Lukasz <luki_9@wp.pl>
2014-02-03 07:24:59 +0100

    Add admin controller file

---
+++ controller/admin_controller.php
@@ -0,0 +1,108 @@
+<?php
+/**
+*
+* @package Board Rules Extension
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbb\boardrules\controller;
+
+/**
+* Admin controller
+*/
+class admin controller implements admin_interface
+{
+	/**
+	* Display the options a user can configure for this extension
+	*
+	* @return null
+	* @access public
+	*/
+	public function display_options()
+	{
+	}
+
+	/**
+	* Set the options a user can configure
+	*
+	* @return null
+	* @access public
+	*/
+	public function set_options()
+	{
+	}
+
+	/**
+	* Display the language selection
+	*
+	* Display the available languages to add/manage board rules from.
+	* If there is only one board language, this will just call display_rules().
+	*
+	* @return null
+	* @access public
+	*/
+	public function display_language_selection()
+	{
+	}
+
+	/**
+	* Display the rules
+	*
+	* @param int $language Language selection identifier; default: 0
+	* @param int $parent_id Category to display rules from; default: 0
+	* @return null
+	* @access public
+	*/
+	public function display_rules($language = 0, $parent_id = 0)
+	{
+	}
+
+	/**
+	* Add a rule
+	*
+	* @param int $language Language selection identifier; default: 0
+	* @param int $parent_id Category to display rules from; default: 0
+	* @return null
+	* @access public
+	*/
+	public function add_rule($language = 0, $parent_id = 0)
+	{
+	}
+
+	/**
+	* Edit a rule
+	*
+	* @param int $rule_id The rule identifier to edit
+	* @return null
+	* @access public
+	*/
+	public function edit_rule($rule_id)
+	{
+	}
+
+	/**
+	* Delete a rule
+	*
+	* @param int $rule_id The rule identifier to delete
+	* @return null
+	* @access public
+	*/
+	public function delete_rule($rule_id)
+	{
+	}
+
+	/**
+	* Move a rule up/down
+	*
+	* @param int $rule_id The rule identifier to move
+	* @param string $direction The direction (up|down)
+	* @param int $amount The number of places to move the rule
+	* @return null
+	* @access public
+	*/
+	public function move_rule($rule_id, $direction, $amount = 1)
+	{
+	}
+}
