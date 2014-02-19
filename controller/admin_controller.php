59e4a903efc43fd272530c1bfd1a86c2482c4cf8
Lukasz <luki_9@wp.pl>
2014-02-19 20:18:23 +0100

    Add navigation
    Replace U_BACK

--- controller/admin_controller.php
+++ controller/admin_controller.php
@@ -26,27 +26,41 @@ class admin_controller implements admin_interface
 	/** @var \phpbb\template\template */
 	protected $template;
 
+	/** @var \phpbb\user */
+	protected $user;
+
 	/** @var \phpbb\boardrules\operators\rule */
 	protected $rule_operator;
 
 	/**
+	* The database table the rules are stored in
+	*
+	* @var string
+	*/
+	protected $boardrules_table;
+
+	/**
 	* Constructor
 	*
-	* @param \phpbb\config\config              $config           Config object
-	* @param \phpbb\db\driver\driver           $db               Database object
-	* @param \phpbb\request\request            $request          Request object
-	* @param \phpbb\template\template          $template         Template object
-	* @param \phpbb\boardrules\operators\rule  $rule_operator    Rule operator object
+	* @param \phpbb\config\config              $config            Config object
+	* @param \phpbb\db\driver\driver           $db                Database object
+	* @param \phpbb\request\request            $request           Request object
+	* @param \phpbb\template\template          $template          Template object
+	* @param \phpbb\user                       $user              User object
+	* @param \phpbb\boardrules\operators\rule  $rule_operator     Rule operator object
+	* @param string                            $boardrules_table  Name of the table used to store boardrules data
 	* @return \phpbb\boardrules\controller\admin_controller
 	* @access public
 	*/
-	public function __construct(\phpbb\config\config $config, \phpbb\db\driver\driver $db, \phpbb\request\request $request, \phpbb\template\template $template, \phpbb\boardrules\operators\rule $rule_operator)
+	public function __construct(\phpbb\config\config $config, \phpbb\db\driver\driver $db, \phpbb\request\request $request, \phpbb\template\template $template, \phpbb\user $user, \phpbb\boardrules\operators\rule $rule_operator, $boardrules_table)
 	{
 		$this->config = $config;
 		$this->db = $db;
 		$this->request = $request;
 		$this->template = $template;
+		$this->user = $user;
 		$this->rule_operator = $rule_operator;
+		$this->$boardrules_table = $boardrules_table;
 	}
 
 	/**
@@ -139,7 +153,6 @@ class admin_controller implements admin_interface
 				'RULE_IMAGE'		=> $is_category ? '<img src="images/icon_subfolder.gif" />' : '<img src="images/icon_folder.gif" />',
 
 				'U_ADD'				=> "{$this->u_action}&amp;language={$language}&amp;parent_id={$parent_id}&amp;action=add",
-				'U_BACK'			=> "{$this->u_action}&amp;language={$language}&amp;parent_id=" . $entity-get_parent_id(),
 				'U_DELETE'			=> "{$this->u_action}&amp;action=delete&amp;rule_id=" . $entity->get_id(),
 				'U_EDIT'			=> "{$this->u_action}&amp;action=edit&amp;rule_id=" . $entity->get_id(),
 				'U_MOVE_DOWN'		=> "{$this->u_action}&amp;action=move_down&amp;rule_id=" . $entity->get_id(),
@@ -149,5 +162,40 @@ class admin_controller implements admin_interface
 
 			$last_right_id = $entity->get_right_id();
 		}
+
+		// Prepare navigation
+		if (!$parent_id)
+		{
+			$navigation = $this->user->lang['BOARDRULES_INDEX'];
+		}
+		else
+		{
+			$navigation = '<a href="' . $this->u_action . '">' . $this->user->lang['BOARDRULES_INDEX'] . '</a>';
+
+			$sql = 'SELECT r2.*
+				FROM ' . $this->boardrules_table . ' r1
+				LEFT JOIN ' . $this->boardrules_table . " r2
+					ON r1.left_id BETWEEN r2.left_id AND r2.right_id
+				WHERE r1.rule_id = $parent_id
+				ORDER BY r2.left_id ASC";
+			$result = $this->db->sql_query($sql);
+
+			while ($row = $this->db->sql_fetchrow($result))
+			{
+				if ($row['rule_id'] == $parent_id)
+				{
+					$navigation .= ' -&gt; ' . $row['rule_title'];
+				}
+				else
+				{
+					$navigation .= ' -&gt; <a href="' . $this->u_action . '&amp;language=' . $language . '&amp;parent_id=' . $row['parent_id'] . '">' . $row['rule_title'] . '</a>';
+				}
+			}
+			$db->sql_freeresult($result);
+		}
+
+		$this-template->assign_vars(array(
+			'NAVIGATION'	=> $navigation,
+		));
 	}
 }
