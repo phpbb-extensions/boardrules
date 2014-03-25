e7ad163c4c8182c30976273c1d84b859c559e613
Matt Friedman <maf675@gmail.com>
2014-03-25 15:54:47 -0700

    Try a functional test for viewonline

---
+++ tests/functional/viewonline_test.php
@@ -0,0 +1,64 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbb\boardrules\tests\functional;
+
+/**
+* @group functional
+*/
+class viewonline_test extends \extension_functional_test_case
+{
+	public function setUp()
+	{
+		parent::setUp();
+		$this->login();
+		$this->admin_login();
+		$this->set_extension('phpbb', 'boardrules', 'Board Rules');
+		$this->enable_extension();
+		$this->enable_boardrules();
+		$this->add_lang_ext(array('boardrules_common', 'boardrules_controller'));
+	}
+
+	/**
+	* Board rules installs in a disabled state. We need to turn it on to test it.
+	*
+	* @access public
+	*/
+	public function enable_boardrules()
+	{
+		$this->get_db();
+
+		$sql = "UPDATE phpbb_config
+			SET config_value = '1'
+			WHERE config_name = 'boardrules_enable'";
+
+		$this->db->sql_query($sql);
+	}
+
+	/**
+	* Test viewonline page
+	*
+	* @access public
+	*/
+	public function test_viewonline_page()
+	{
+		$this->create_user('user1');
+		$this->login('user1');
+		$crawler = self::request('GET', 'app.php/rules');
+
+		$this->sid = null;
+
+		$this->create_user('user2');
+		$this->login('user2');
+		$crawler = self::request('GET', 'viewonline.php');
+
+		$this->assertContains('user1', $crawler->text());
+		$this->assertContains($this->lang('BOARDRULES_VIEWONLINE'), $crawler->text());
+	}
+}
