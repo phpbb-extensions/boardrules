1df785e5aae5f9775b4b6ff5fda8fc9e2d388b8a
Pico88 <luki_9@wp.pl>
2014-03-30 13:02:34 +0200

    Add acp module file
    Define some global vars

---
+++ acp/boardrules_module.php
@@ -0,0 +1,42 @@
+<?php
+/**
+*
+* @package Board Rules Extension
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbb\boardrules\acp;
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+class boardrules_module
+{
+	var $u_action;
+
+	function main($id, $mode)
+	{
+		global $db, $user, $auth, $template, $cache, $request;
+		global $config, $phpbb_root_path, $phpbb_admin_path, $phpEx, $phpbb_container;
+
+		switch($action)
+		{
+		}
+
+		switch($mode)
+		{
+			case 'settings':
+			break;
+
+			case 'manage':
+			break;
+		}
+	}
+}
