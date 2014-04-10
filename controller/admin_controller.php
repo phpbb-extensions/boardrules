f9d0d0a8e4de4b989a5317d0cd84f7f971e13aeb
Matt Friedman <maf675@gmail.com>
2014-04-10 16:37:14 -0700

    Remove the dots from the link hashes

--- controller/admin_controller.php
+++ controller/admin_controller.php
@@ -185,8 +185,8 @@ class admin_controller implements admin_interface
 
 				'U_DELETE'			=> "{$this->u_action}&amp;action=delete&amp;rule_id=" . $entity->get_id(),
 				'U_EDIT'			=> "{$this->u_action}&amp;action=edit&amp;rule_id=" . $entity->get_id(),
-				'U_MOVE_DOWN'		=> "{$this->u_action}&amp;action=move_down&amp;rule_id=" . $entity->get_id() . '&amp;hash=' . generate_link_hash('down.' . $entity->get_id()),
-				'U_MOVE_UP'			=> "{$this->u_action}&amp;action=move_up&amp;rule_id=" . $entity->get_id() . '&amp;hash=' . generate_link_hash('up.' . $entity->get_id()),
+				'U_MOVE_DOWN'		=> "{$this->u_action}&amp;action=move_down&amp;rule_id=" . $entity->get_id() . '&amp;hash=' . generate_link_hash('down' . $entity->get_id()),
+				'U_MOVE_UP'			=> "{$this->u_action}&amp;action=move_up&amp;rule_id=" . $entity->get_id() . '&amp;hash=' . generate_link_hash('up' . $entity->get_id()),
 				'U_RULE'			=> "{$this->u_action}&amp;language={$language}&amp;parent_id=" . $entity->get_id(),
 			));
 
@@ -415,7 +415,7 @@ class admin_controller implements admin_interface
 	*/
 	public function move_rule($rule_id, $direction, $amount = 1)
 	{
-		if (!check_link_hash($this->request->variable('hash', ''), $direction . '.' . $rule_id))
+		if (!check_link_hash($this->request->variable('hash', ''), $direction . $rule_id))
 		{
 			trigger_error($this->user->lang['FORM_INVALID'] . adm_back_link($this->u_action));
 		}
