55b63e392c314a1b69cd030a054b301cb844c23b
Pico88 <luki_9@wp.pl>
2014-05-04 08:34:19 +0200

    Correct desc for rule data field

--- controller/admin_controller.php
+++ controller/admin_controller.php
@@ -317,7 +317,7 @@ class admin_controller implements admin_interface
 
 		unset($message_parse_options);
 
-		// Grab the form data's rule
+		// Grab the form's rule data fields
 		$rule_fields = array(
 			'title'		=> $data['rule_title'],
 			'anchor'	=> $data['rule_anchor'],
