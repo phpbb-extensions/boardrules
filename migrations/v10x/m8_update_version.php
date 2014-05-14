a2cfc3018c2de9c62298b203c3ccec2f964f9dc2
Matt Friedman <maf675@gmail.com>
2014-05-14 07:59:55 -0700

    Update version to 1.0.0

---
+++ migrations/v10x/m8_update_version.php
@@ -0,0 +1,41 @@
+<?php
+/**
+*
+* @package Board Rules Extension
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbb\boardrules\migrations\v10x;
+
+/**
+* Migration stage 8: Update version
+*/
+class m8_update_version extends \phpbb\db\migration\migration
+{
+	/**
+	* Assign migration file dependencies for this migration
+	*
+	* @return array Array of migration files
+	* @static
+	* @access public
+	*/
+	static public function depends_on()
+	{
+		return array('\phpbb\boardrules\migrations\v10x\m7_sample_rule_data');
+	}
+
+	/**
+	* Add or update data in the database
+	*
+	* @return array Array of table data
+	* @access public
+	*/
+	public function update_data()
+	{
+		return array(
+			array('config.update', array('boardrules_version', '1.0.0')),
+		);
+	}
+}
