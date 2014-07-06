29b2fa117f7d234850650f86049dd8aa0c7f00ef
Matt Friedman <maf675@gmail.com>
2014-07-06 14:44:12 -0700

    Remove un-needed methods from migration

--- migrations/v10x/m10_notification_counter.php
+++ migrations/v10x/m10_notification_counter.php
@@ -16,30 +16,6 @@ namespace phpbb\boardrules\migrations\v10x;
 class m10_notification_counter extends \phpbb\db\migration\migration
 {
 	/**
-	* Check if this migration is effectively installed
-	*
-	* @return bool True if config name does not exist, false otherwise
-	* @static
-	* @access public
-	*/
-	public function effectively_installed()
-	{
-		return isset($this->config['boardrules_notification']);
-	}
-
-	/**
-	* Assign migration file dependencies for this migration
-	*
-	* @return array Array of migration files
-	* @static
-	* @access public
-	*/
-	static public function depends_on()
-	{
-		return array('\phpbb\boardrules\migrations\v10x\m1_initial_schema');
-	}
-
-	/**
 	* Add or update data in the database
 	*
 	* @return array Array of table data
