16e147b73387e448da58a32eaa14becef9129b15
Joas Schilling <nickvergessen@gmx.de>
2014-07-09 00:57:40 +0200

    Correctly create a second session and check for the admin users page info

--- tests/functional/viewonline_test.php
+++ tests/functional/viewonline_test.php
@@ -52,30 +52,54 @@ class viewonline_test extends \phpbb_functional_test_case
 	}
 
 	/**
-	* Test viewonline page
+	* Visit rules page as user "admin"
 	*
 	* @access public
 	*/
-	public function test_viewonline_page()
+	public function test_viewonline_visit_rules()
 	{
-		$this->markTestSkipped('Travis is returning an unexpected "session_page" rules/rules instead of app.php/rules');
-
 		// Send the admin to the Rules page
 		$this->login();
-		$crawler = self::request('GET', 'app.php/rules?sid={$this->sid}');
+		$crawler = self::request('GET', "app.php/rules?sid={$this->sid}");
 		$this->assertContains($this->lang('BOARDRULES_HEADER'), $crawler->filter('h2')->text());
+	}
 
-		// Allow us to create and login a second user
-		$this->sid = null;
-		self::$cookieJar->clear();
-
+	/**
+	* Test viewonline page for admin
+	*
+	* We use a second function here, so we get a new session and can login
+	* without having to log out "admin" first.
+	*
+	* @access  public
+	* @depends test_viewonline_visit_rules
+	*/
+	public function test_viewonline_check_viewonline()
+	{
 		// Create user1 and send them to the Viewonline
 		$this->create_user('user1');
 		$this->login('user1');
-		$crawler = self::request('GET', 'viewonline.php?sid={$this->sid}');
+		$crawler = self::request('GET', "viewonline.php?sid={$this->sid}");
 
 		// Is admin still viewing Rules page
-		$this->assertContains('admin', $crawler->text());
-		$this->assertContains($this->lang('BOARDRULES_VIEWONLINE'), $crawler->filter('.info')->text());
+		$this->assertContains('admin', $crawler->filter('#page-body table.table1')->text());
+
+		$session_entries = $crawler->filter('#page-body table.table1 tr')->count();
+		$this->assertGreaterThanOrEqual(3, $session_entries, 'Too few session entries found');
+
+		// Check each entry in the viewonline table
+		// Skip the first row (header)
+		for ($i = 1; $i < $session_entries; $i++)
+		{
+			// If we found the admin, we check his page info and leave
+			$subcrawler = $crawler->filter('#page-body table.table1 tr')->eq($i);
+			if (strpos($subcrawler->filter('td')->text(), 'admin') !== false)
+			{
+				$this->assertContains($this->lang('BOARDRULES_VIEWONLINE'), $subcrawler->filter('td.info')->text());
+				return;
+			}
+		}
+
+		// If we did not find the admin, we fail
+		$this->fail('User "admin" was not found on viewonline page.');
 	}
 }
