8a3a995fe1d659fdd3bac5339c982e6139bbc9f8
Matt Friedman <maf675@gmail.com>
2014-11-04 12:08:28 -0800

    Fix a copyright year in NL lang pack

--- language/nl/boardrules_common.php
+++ language/nl/boardrules_common.php
@@ -4,7 +4,7 @@
 * Board Rules extension for the phpBB Forum Software package.
 * Dutch translation by Dutch Translators (https://github.com/dutch-translators)
 *
-* @copyright (c) 2013 phpBB Limited <https://www.phpbb.com>
+* @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
