eebde053d6b8988309142879b038aa1d8ce03545
Raphaël <Galixte@users.noreply.github.com>
2014-12-24 13:25:05 +0100

    Update for FRENCH translation for permissions_boardrules.php
    Update for FRENCH translation for permissions_boardrules.php

--- language/fr/permissions_boardrules.php
+++ language/fr/permissions_boardrules.php
@@ -2,7 +2,7 @@
 /**
 *
 * Board Rules extension for the phpBB Forum Software package.
-* French translation by ForumsFaciles (http://www.forumsfaciles.fr)
+* French translation by ForumsFaciles (http://www.forumsfaciles.fr) & Galixte (http://www.galixte.com)
 *
 * @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
 * @license GNU General Public License, version 2 (GPL-2.0)
@@ -39,5 +39,5 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'ACL_A_BOARDRULES'		=> 'Peut gérer les Règles du forum',
+	'ACL_A_BOARDRULES'		=> 'Peut gérer les règles du forum',
 ));
