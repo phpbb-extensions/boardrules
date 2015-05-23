ee6f9086c9f050f11e138a49211397b20e5e47df
Matt Friedman <maf675@gmail.com>
2015-05-23 07:15:59 -0700

    Update comments [ci skip]

--- migrations/v10x/m3_add_schema.php
+++ migrations/v10x/m3_add_schema.php
@@ -30,7 +30,7 @@ class m3_add_schema extends \phpbb\db\migration\migration
 	/**
 	* Add table columns schema to the database:
 	*    boardrules:
-	*        rule_parents Basic rule data is serialized and cached here
+	*        rule_parents Basic rule data is serialised and cached here
 	*
 	* @return array Array of table columns schema
 	* @access public
