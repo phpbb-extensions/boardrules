48d51c432820dafab5fcfb3f70253555ef24f97b
Matt Friedman <maf675@gmail.com>
2015-10-25 13:07:55 -0700

    Run reparser via migration

---
+++ migrations/v20x/m14_reparse.php
@@ -0,0 +1,66 @@
+<?php
+/**
+*
+* Board Rules extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2015 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace phpbb\boardrules\migrations\v20x;
+
+class m14_reparse extends \phpbb\db\migration\container_aware_migration
+{
+	/**
+	 * @inheritDoc
+	 */
+	static public function depends_on()
+	{
+		return array(
+			'\phpbb\boardrules\migrations\v10x\m7_sample_rule_data',
+			'\phpbb\boardrules\migrations\v20x\m13_font_icon',
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function update_data()
+	{
+		return array(
+			array('custom', array(array($this, 'reparse'))),
+		);
+	}
+
+	/**
+	 * Run the boardrules rule text reparser
+	 *
+	 * @param int $current A rule identifier
+	 * @return bool|int A rule identifier or true if finished
+	 */
+	public function reparse($current = 0)
+	{
+		$reparser = $this->container->get('phpbb.boardrules.text_reparser.rule_text');
+
+		if (empty($current))
+		{
+			$current = $reparser->get_max_id();
+		}
+
+		$limit = 50; // lets keep the reparsing conservative
+		$processed_records = 0;
+		while ($processed_records < $limit && $current > 0)
+		{
+			$start = max(1, $current + 1 - ($limit - $processed_records));
+			$end   = max(1, $current);
+
+			$reparser->reparse_range($start, $end);
+
+			$processed_records = $end - $start + 1;
+			$current = $start - 1;
+		}
+
+		return ($current === 0) ? true : $current;
+	}
+}
