e315966cf06acaebe1263d95b20d372871549f29
Matt Friedman <maf675@gmail.com>
2022-01-04 21:47:01 -0800

    Update for use with non-local iconify

--- adm/style/boardrules.js
+++ adm/style/boardrules.js
@@ -2,15 +2,22 @@
 
 	'use strict';
 
+	/* global Iconify */
 	$('#boardrules_font_icon').on('keyup blur', function() {
 		var input = $(this).val();
 		var $icon = $(this).next('svg');
 		if (input.length > 0) {
 			input = input.indexOf(':') === -1 ? 'fa:' + input : input;
-			if (Iconify.iconExists(input)) {
-				$icon.remove().end().after(Iconify.renderHTML(input));
-				return;
-			}
+			Iconify.loadIcons([input], (loaded, missing, pending, unsubscribe) => {
+				if (loaded.length) {
+					$icon.remove().end().after(Iconify.renderHTML(input));
+					return;
+				}
+
+				if (missing.length) {
+					$icon.remove();
+				}
+			});
 		}
 
 		$icon.remove();
